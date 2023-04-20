e6021216278a0be8246af277633366df15b23569
Malthe <malthegram22@gmail.com>
2023-04-20 11:10:58 +0200

    Moved files around

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
 
 
-    import Footer from "../lib/Footer.svelte";
-    import Header from "../lib/Header.svelte";
+    import Footer from "../lib/components/Footer.svelte";
+    import Header from "../lib/components/Header.svelte";
 </script>
 
 <Header/>
