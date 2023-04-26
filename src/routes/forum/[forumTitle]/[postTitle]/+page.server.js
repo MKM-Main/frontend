7c92feb68eebd2ac675453d0ddb03631da7347a7
Malthe <malthegram22@gmail.com>
2023-04-26 13:57:09 +0200

    forumv3 yolo

---
+++ src/routes/forum/[forumTitle]/[postTitle]/+page.server.js
@@ -0,0 +1,16 @@
+export const load = async ({fetch, params, cookies}) => {
+    const jwt = cookies.get("jwt")
+    
+    function replaceHyphensWithSpaces(str) {
+        return str.replace(/-/g, " ");
+      } 
+
+      const postTitle = replaceHyphensWithSpaces(params.postTitle)
+      const postResponse = await fetch(`http://localhost:8080/forum/post/${postTitle}`);
+    
+    const postData = await postResponse.json()
+    return {
+        json: postData,
+        cookie: jwt
+    }
+}
