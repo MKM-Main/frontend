fb297082b4c54e1da5de4b8126e08a5cc66bf7be
MichaelDyvad <michaeldyvad@gmail.com>
2023-05-01 11:13:43 +0200

    added functionality to profile comments

---
+++ src/routes/profile/[artistName]/Comment.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let comment;
+  </script>
+  
+  <div class="comment">
+    <b><p>{comment.commentedBy}</p></b>
+    <p>Comment: {comment.body}</p>
+    <p>{comment.timeStamp}</p>
+  </div>
+
+<style>
+    .comment{
+        border: 2px solid black;
+        border-radius: 15px;
+        padding: 10px;
+        background-color: #F0F2F5;
+        margin-top: 5px;
+    }
+</style>
