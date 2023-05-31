4a878270852f68e46b2c8b913aa5542284efb9b9
Malthe <malthegram22@gmail.com>
2023-05-31 11:26:15 +0200

    update adapter for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,17 @@
-
 // svelte.config.js
 
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({ scss: true }),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess({scss: true}),
 
-	kit: {
-		adapter: adapter(),
-	},
+    kit: {
+        adapter: adapter(),
+    },
 };
 
 export default config;
